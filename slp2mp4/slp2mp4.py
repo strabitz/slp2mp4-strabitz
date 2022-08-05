c6714ad3ed417e45ee72bcc10a6d92458b23cf77
unknown <davisclaib@gmail.com>
2022-08-05 18:33:15 -0500

    Set up pip installation

--- slp2mp4/slp2mp4.py
+++ slp2mp4/slp2mp4.py
@@ -6,7 +6,7 @@ from config import Config
 from dolphinrunner import DolphinRunner
 from ffmpegrunner import FfmpegRunner
 
-VERSION = '1.0.0'
+VERSION = '2.0.0'
 USAGE = """\
 slp-to-mp4 {}
 Convert slippi files to mp4 videos
